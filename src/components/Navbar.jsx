6d639e899ca0c235f3dc86092d8786af15599f2b
ROGUE-DK <divyeshkhunt08@gmail.com>
2023-09-07 19:52:20 +0530

    Add files to the repository

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Navcards.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-list">
         <li className="nav-item"><a href="/">Cartoons</a></li>
-        <li className="nav-item"><a href="/games">Games<FontAwesomeIcon icon="fa-solid fa-gamepad" /></a></li>
+        <li className="nav-item"><a href="/games">Games</a></li>
         <li className="nav-item"><a href="/about">About Us</a></li>
       </ul>
     </nav>
